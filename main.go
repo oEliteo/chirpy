7e3c5272ad2704bf4d5b034073e1ef2d9fd0f4a4
oeliteo <woolseytrindon@gmail.com>
2026-06-05 19:17:27 -0600

    added request counting middleware and reset. application state struct and some other things.

--- main.go
+++ main.go
@@ -1,16 +1,56 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-func main() {
-	sMux := http.NewServeMux()
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
-	sMux.Handle("/", http.FileServer(http.Dir(".")))
-	httpServer := http.Server{
-		Addr:    ":8080",
-		Handler: sMux,
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	cfg := apiConfig{}
+	port := "8080"
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fs)))
+	mux.HandleFunc("/metrics", cfg.metricsHandler)
+	mux.HandleFunc("/healthz", checkServerStatus)
+	mux.HandleFunc("/reset", cfg.resetHandler)
+	srv := http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
 	}
-	httpServer.ListenAndServe()
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func checkServerStatus(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	text := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	w.Write([]byte(text))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
