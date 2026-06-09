0c82725cb45ae2ea7ab589b69c285c5cddeb3356
oeliteo <woolseytrindon@gmail.com>
2026-06-09 17:02:08 -0600

    added chirp validation endpoint

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -24,9 +26,10 @@ func main() {
 	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("."))
 	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", fs)))
-	mux.HandleFunc("/metrics", cfg.metricsHandler)
-	mux.HandleFunc("/healthz", checkServerStatus)
-	mux.HandleFunc("/reset", cfg.resetHandler)
+	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler)
+	mux.HandleFunc("GET /api/healthz", checkServerStatus)
+	mux.HandleFunc("POST /admin/reset", cfg.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", cfg.validateChirp)
 	srv := http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -38,13 +41,13 @@ func main() {
 func checkServerStatus(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+	w.Write([]byte(http.StatusText(http.StatusOK) + "\n"))
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	text := fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())
+	text := fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits.Load())
 	w.Write([]byte(text))
 }
 
@@ -52,5 +55,73 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+	w.Write([]byte(http.StatusText(http.StatusOK) + "\n"))
+}
+
+func (cfg *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
+	type args struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := args{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		cfg.respondWithError(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		cfg.respondWithError(w, fmt.Errorf("Chirp is too long"), http.StatusBadRequest)
+		return
+	}
+
+	type successResponse struct {
+		Valid bool `json:"valid"`
+	}
+
+	successResp := successResponse{
+		Valid: true,
+	}
+
+	dat, err := json.Marshal(successResp)
+	if err != nil {
+		cfg.respondWithError(w, err, http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(dat)
+}
+
+func (cfg *apiConfig) respondWithError(w http.ResponseWriter, e error, status int) {
+	type requestError struct {
+		Msg string `json:"error"`
+	}
+	dat := make([]byte, 0)
+	err := errors.New("")
+	reqErr := requestError{}
+	switch status {
+	case http.StatusInternalServerError:
+		reqErr.Msg = fmt.Sprintf("Internal Server Error: %v", status)
+		dat, err = json.Marshal(reqErr)
+	case http.StatusBadRequest:
+		reqErr.Msg = fmt.Sprintf("Bad Request: %s", e)
+		dat, err = json.Marshal(reqErr)
+	default:
+		reqErr.Msg = "An Unknown Error Has Occured"
+		dat, err = json.Marshal(reqErr)
+	}
+
+	if err != nil {
+		fmt.Printf("Error Marshalling reqErr: %s", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("An unknown error has occured"))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(dat)
 }
